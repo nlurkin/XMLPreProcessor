cd1c88a645f5b6c40261c96d4b307ee1928aeb38
nlurkin <nlurkin@debian>
2015-04-19 01:20:00 +0100

    Document the C interface with example

--- Example/main_proxy.cc
+++ Example/main_proxy.cc
@@ -7,12 +7,46 @@
 
 #include "ex_struct.h"
 #include "ex_structProxy.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define PRINTVAR(v) #v << "= " << v << " "
 
 int main(){
 	exampleStruct test;
 
-	//test.a = 5;
-	//test.b.a = 2;
+	xml_read_file("ex_struct.xml");
+	xml_apply_exampleStruct(&test);
 
-	xml_create_exampleStruct(&test, "ex_struct.xml");
+	xml_read_file("partial_ex_struct.xml");
+	xml_apply_exampleStruct(&test);
+
+	cout << "Differences:" << endl;
+	xml_test_exampleStruct();
+	cout << endl;
+
+	void* ptr = xml_start_compare_exampleStruct(&test);
+
+	while(ptr!=NULL){
+		if(ptr==&(test.version)) cout << "mod: test.version was modified" << endl;
+		else if(ptr==&(test.name)) cout << "mod: test.name was modified" << endl;
+		else if(ptr==&(test.my_substruct.my_double)) cout << "mod: test.my_substruct.my_double was modified" << endl;
+		else if(ptr==&(test.my_substruct.my_integer)) cout << "mod: test.my_substruct.my_integer was modified" << endl;
+		else if(ptr==&(test.my_array[2])) cout << "mod: test.my_array[2] was modified" << endl;
+		ptr = xml_next_compare_exampleStruct(&test);
+	}
+
+	std::cout << "val: " << PRINTVAR(test.version) << endl;
+	std::cout << "val: " << PRINTVAR(test.name) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_array[0]) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_array[1]) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_array[2]) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_array[3]) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_array[4]) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_substruct.my_double) << endl;
+	std::cout << "val: " << PRINTVAR(test.my_substruct.my_integer) << endl;
+
+	strcpy(test.name, "my_modified_struct");
+	xml_create_exampleStruct(&test, "outputStruct.xml");
 }
--- Example/run_all_examples.sh
+++ Example/run_all_examples.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=../XMLConf:$LD_LIBRARY_PATH
-./example_proxy_create
+outproxy=`./example_proxy` 
 ./example_write
-out=`./example_parse`
+outparse=`./example_parse`
 
 if [ ! -d DocGen ]; then 
 	mkdir DocGen
 fi
 
-echo "$out" | grep "pe:"  > DocGen/parser_path_exist.txt
-echo "$out" | grep "d:"  > DocGen/parser_diff.txt
-echo "$out" | grep -v "pe:" | grep -v "d:"  > DocGen/parser_add.txt
+echo "$outproxy" | grep "val:" > DocGen/proxy_values.txt
+echo "$outproxy" | grep "mod:" > DocGen/proxy_mod.txt
+echo "$outproxy" | grep -v "val:" | grep -v "mod:" > DocGen/proxy_diff.txt
+
+echo "$outparse" | grep "pe:"  > DocGen/parser_path_exist.txt
+echo "$outparse" | grep "d:"  > DocGen/parser_diff.txt
+echo "$outparse" | grep -v "pe:" | grep -v "d:"  > DocGen/parser_add.txt
 
 echo "/*! \page list_pe" > DocGen/parser_list_pe.dox
 cat main_parser.cc | grep "pe:" | sed "s/.*pe: \(.*\) is not.*/ - \1/" >> DocGen/parser_list_pe.dox
